35161f3b76f721676ad7d3f1cef0d080a18ef60f
Craig Granger <105527772+ComputerDesignedGraphics@users.noreply.github.com>
2023-11-03 18:11:00 -0600

    Move, Jump, Will NOT climb ladders, fixed walking animation - working on idle
    Need help :(
    Move, Jump, Will NOT climb ladders, fixed walking animation - working on idle player still will not flip yet another script had it flipping - checking that script to see if I can use it instead

--- SideScrollerGame/SideScrollerGame/Assets/Maya/scripts/CharacterMovement.cs
+++ SideScrollerGame/SideScrollerGame/Assets/Maya/scripts/CharacterMovement.cs
@@ -3,25 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
-
 {
     public float moveSpeed = 5f;
     public float jumpForce = 700f;
     private bool isGrounded = false;
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
+    private bool climbing = false;
     
     // LayerMask to determine what is considered ground for the player
     public LayerMask groundLayer;
-    
-    // Start is called before the first frame update
+    public LayerMask ladderLayer; // Make sure you create a layer for the ladder and assign it to your ladder GameObjects
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         // Movement left and right
@@ -31,35 +30,58 @@ public class CharacterMovement : MonoBehaviour
         // Flip the character when moving left/right
         if (move > 0 && spriteRenderer.flipX)
         {
+            Debug.Log("Flipping to right");
             spriteRenderer.flipX = false;
         }
         else if (move < 0 && !spriteRenderer.flipX)
         {
+            Debug.Log("Flipping to left");
             spriteRenderer.flipX = true;
         }
 
+
+        // Climbing
+        if (climbing)
+        {
+            float climb = Input.GetAxis("Vertical") * moveSpeed;
+            rb.velocity = new Vector2(rb.velocity.x, climb);
+            rb.gravityScale = 0; // Disable gravity while climbing
+            
+        }
+        else
+        {
+            rb.gravityScale = 1; // Enable gravity when not climbing
+        }
+
         // Jumping
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded && !climbing)
         {
             rb.AddForce(new Vector2(0f, jumpForce));
         }
     }
 
-    // Check if the player is grounded
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground") || (groundLayer.value & (1 << collision.gameObject.layer)) > 0)
+        if ((groundLayer.value & (1 << collision.gameObject.layer)) > 0)
         {
             isGrounded = true;
         }
+        else if ((ladderLayer.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            climbing = true;
+            
+        }
     }
 
-    // Check if the player has left the ground
     void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground") || (groundLayer.value & (1 << collision.gameObject.layer)) > 0)
+        if ((groundLayer.value & (1 << collision.gameObject.layer)) > 0)
         {
             isGrounded = false;
+        }
+        else if ((ladderLayer.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            climbing = false;
         }
     }
 }
