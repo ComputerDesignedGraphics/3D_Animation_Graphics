d55edda669ef944e4aed4b4b8b280eeb710e3998
Craig Granger <105527772+ComputerDesignedGraphics@users.noreply.github.com>
2023-11-18 10:23:10 -0700

    MainMenu Complete
    Play, Options, Quit along with Volume slider all working - will add actual volume and click sounds at a later date

--- SideScrollerGame/SideScrollerGame/Assets/Scripts/MainMenu.cs
+++ SideScrollerGame/SideScrollerGame/Assets/Scripts/MainMenu.cs
@@ -1,20 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
-    public class MainMenu : MonoBehaviour
+    public void PlayGame ()
     {
-        public void PlayGame()
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-
-        public void QuitGame()
-        {
-            Application.Quit();
-        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
 }
